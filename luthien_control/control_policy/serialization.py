ddc0450d5a3986e3ba0a46da59cfc88a6665e56b
JaiDhyani <jaiwithani@gmail.com>
2025-04-22 16:45:37 -0700

    rm unused

--- luthien_control/control_policy/serialization.py
+++ luthien_control/control_policy/serialization.py
@@ -5,11 +5,3 @@ T = TypeVar("T", bound="SerializableDict")
 SerializableType = Union[str, float, int, "SerializableDict"]
 
 SerializableDict = Dict[str, SerializableType]
-
-class PolicyClassNames:
-    """Class names for control policies."""
-
-    ADD_API_KEY_HEADER = "AddApiKeyHeaderPolicy"
-    SEND_BACKEND_REQUEST = "SendBackendRequestPolicy"
-    REQUEST_LOGGING = "RequestLoggingPolicy"
-    
